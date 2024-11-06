019a82269331757329fd2d8f8cc5e1c5416045da
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-11-06 09:13:05 +0800

    test connect database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
 
 }
