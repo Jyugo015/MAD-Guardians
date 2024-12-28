591fef25eebce451b6b38338f24a323101d02229
Jyugo015 <limyewoon.lyw@gmail.com>
2024-12-28 22:37:58 +0800

    notification backend update

--- app/src/main/java/com/example/madguardians/database/Notification.java
+++ app/src/main/java/com/example/madguardians/database/Notification.java
@@ -1,40 +1,16 @@
 package com.example.madguardians.database;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.room.Entity;
-import androidx.room.ForeignKey;
-import androidx.room.PrimaryKey;
-
-@Entity(tableName = "notification",
-        foreignKeys ={
-            @ForeignKey(
-                    entity = User.class,
-                    parentColumns = "userId",
-                    childColumns = "userId",
-                    onDelete = ForeignKey.CASCADE
-            )
-        }
-)
+import com.google.firebase.Timestamp;
 public class Notification {
-    @PrimaryKey
-    @NonNull
     private String notificationId;
-    @NonNull
     private String userId;
-    @NonNull
     private String message;
-    @Nullable
-    private String deliveredTime;
-    @Nullable
-    private String readTime;
+    private Timestamp deliveredTime;
+    private Timestamp readTime;
 
-    public Notification() {
-    }
+    public Notification() {}
 
-    // Constructor
-    public Notification(String notificationId, @NonNull String userId, @NonNull String message,
-                        @Nullable String deliveredTime, @Nullable String readTime) {
+    public Notification(String notificationId, String userId, String message, Timestamp deliveredTime, Timestamp readTime) {
         this.notificationId = notificationId;
         this.userId = userId;
         this.message = message;
@@ -42,45 +18,44 @@ public class Notification {
         this.readTime = readTime;
     }
 
-    // Getters
+    // Getter and Setter
     public String getNotificationId() {
         return notificationId;
+    }
+
+    public void setNotificationId(String notificationId) {
+        this.notificationId = notificationId;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public String getDeliveredTime() {
-        return deliveredTime;
-    }
-
-    public String getReadTime() {
-        return readTime;
-    }
-
-    // Setters
-    public void setNotificationId(String notificationId) {
-        this.notificationId = notificationId;
-    }
-
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     public void setMessage(String message) {
         this.message = message;
     }
 
-    public void setDeliveredTime(String deliveredTime) {
+    public Timestamp getDeliveredTime() {
+        return deliveredTime;
+    }
+
+    public void setDeliveredTime(Timestamp deliveredTime) {
         this.deliveredTime = deliveredTime;
     }
 
-    public void setReadTime(String readTime) {
+    public Timestamp getReadTime() {
+        return readTime;
+    }
+
+    public void setReadTime(Timestamp readTime) {
         this.readTime = readTime;
     }
 }
